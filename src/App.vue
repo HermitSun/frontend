05733078d761dcb4eac37ef9d177776d13a98149
Wen Sun <syl1887415157@126.com>
2019-01-24 17:58:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <HelloWorld/>
+        <Test/>
+    </div>
+</template>
+
+<script lang="ts">
+    import HelloWorld from './components/HelloWorld.vue'
+    import Test from './components/Test.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld,
+            Test
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <input v-model="msg">
+        <p>prop: {{ propMessage }}</p>
+        <p>msg: {{ msg }}</p>
+        <p>helloMsg: {{ helloMsg }}</p>
+        <p>computed msg: {{ computedMsg }}</p>
+        <HelloWorld/>
+        <p>
+            <button @click="greet">Greet</button>
+        </p>
+        <p>
+            Clicked: {{ count }} times
+            <button @click="increment">+</button>
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import Component from 'vue-class-component'
+    import HelloWorld from "./HelloWorld.vue";
+
+    @Component({
+        components:{
+            HelloWorld
+        }
+    })
+    export default class Test extends Vue{
+        msg: number = 123
+        // use prop values for initial data
+        helloMsg: string = 'Hello, World!'
+        // annotate refs type
+        // additional declaration is needed
+        // when you declare some properties in `Component` decorator
+        count!: number
+        increment!: () => void
+        // lifecycle hook
+        mounted () {
+            this.greet()
+        }
+        // computed
+        get computedMsg () {
+            return 'computed ' + this.msg
+        }
+        // method
+        greet () {
+            alert('greeting: ' + this.msg)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

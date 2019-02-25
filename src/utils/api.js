8c6d6f2f6998fec6ecaad8b258c70927d595517f
Wen Sun <syl1887415157@126.com>
2019-02-25 10:24:27 +0800

    modify api config

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,40 @@
 import axios from 'axios';
 import {getToken} from "./token.ts";
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'http://localhost:3141';
+axios.defaults.retry = 4;
+axios.defaults.retryDelay = 1000;
+
+//在API文件中
+axios.interceptors.response.use(undefined, function axiosRetryInterceptor(err) {
+    let config = err.config;
+    // If config does not exist or the retry option is not set, reject
+    if (!config || !config.retry) return Promise.reject(err);
+
+    // Set the variable for keeping track of the retry count
+    config.__retryCount = config.__retryCount || 0;
+
+    // Check if we've maxed out the total number of retries
+    if (config.__retryCount >= config.retry) {
+        // Reject with the error
+        return Promise.reject(err);
+    }
+
+    // Increase the retry count
+    config.__retryCount += 1;
+
+    // Create new promise to handle exponential backoff
+    let backoff = new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve();
+        }, config.retryDelay || 1);
+    });
+
+    // Return the promise in which recalls axios to retry the request
+    return backoff.then(function () {
+        return axios(config);
+    });
+});
 
 axios.interceptors.request.use(config => {
         let token = getToken();
